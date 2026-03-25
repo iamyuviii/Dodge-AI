f729fd91739567b9101ae735bff578f0cba585a6
Yuvraj Khichi <yuvraj.khichi@timesinternet.in>
2026-03-26 01:13:32 +0530

    render file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import axios from 'axios'
+
+if (import.meta.env.VITE_API_BASE_URL) {
+  axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
